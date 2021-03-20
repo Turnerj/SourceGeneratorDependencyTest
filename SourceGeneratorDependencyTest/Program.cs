82d0e7f5085c55277783746fc2377b90e1f2b546
Turnerj <git@turnerj.com>
2021-03-21 01:12:49 +1030

    Add project files.

---
+++ SourceGeneratorDependencyTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceGeneratorDependencyTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GeneratedSource.TestProperty);
+		}
+	}
+}
---
+++ SourceGeneratorProject/CustomSourceGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Text.Json;
+
+namespace SourceGeneratorProject
+{
+	[Generator]
+	public class CustomSourceGenerator : ISourceGenerator
+	{
+		public TestObject Object { get; set; }
+
+		public void Execute(GeneratorExecutionContext context)
+		{
+			context.AddSource("TestSource", $@"
+public static class GeneratedSource
+{{
+	public static readonly int TestProperty = {Object.TestProperty};
+}}");
+		}
+
+		public void Initialize(GeneratorInitializationContext context)
+		{
+			Object = JsonSerializer.Deserialize<TestObject>(@"{""TestProperty"":123}");
+		}
+	}
+
+	public class TestObject
+	{
+		public int TestProperty { get; set; }
+	}
+}
